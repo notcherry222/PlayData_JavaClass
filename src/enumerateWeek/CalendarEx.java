296d2be9f9d8dadabe4148f7b155db85de00fadf
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-07-04 14:47:06 +0900

    23.07.04

---
+++ src/enumerateWeek/CalendarEx.java
@@ -0,0 +1,45 @@
+package enumerateWeek;
+
+/**
+ * 대부분 util에 있음
+ */
+import java.util.Calendar;
+
+public class CalendarEx {
+    public static void main(String[] args) {
+
+        Week today = null; // today : 열거 타입 변수, monday~sunday : 열거 상수
+
+        Calendar cal = Calendar.getInstance();
+        int week = cal.get(Calendar.DAY_OF_WEEK);
+
+        switch (week) {
+            case 1:
+                today = Week.SUNDAY;
+                break;
+            case 2:
+                today = Week.MONDAY;
+                break;
+            case 3:
+                today = Week.TUESDAY;
+                break;
+            case 4:
+                today = Week.WEDNESDAY;
+                break;
+            case 5:
+                today = Week.THURSDAY;
+                break;
+            case 6:
+                today = Week.FRIDAY;
+                break;
+            case 7:
+                today = Week.SATURDAY;
+                break;
+        } System.out.println("오늘 요일 : " + today);
+
+        if (today == Week.SUNDAY){
+            System.out.println("DON'T TOUCH ME");
+        } else { System.out.println("DON'T CALL ME");
+        }
+    }
+}
